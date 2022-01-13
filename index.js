746ea1eea9f46c020908a44480b416957a6498ed
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-13 11:17:03 +0100

    filter

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const namen = ['John','Bob', 'Robert', 'Chuck', 'Pete', 'Sammy', 'Ana', 'Sarah',
 // 2 )
 const namen2= ['John', 'Bobby', 'Robert', 'Chuck', 'Peter', 'Sam', 'Ana', 'Sarah', 'Tithany', 'Oscar', 'Henry', 'Greg'];
 // 3 )
-const prüfName =namen2.filter((check) => namen.includes(check));
+const prüfName =namen2.filter((names) => namen.includes(names));
 console.log(prüfName);

045fcf7a4dc6f8c46244156eaf5d2ed739d0857d
H94ssein44 <alali.52@yahoo.com>
2022-01-13 11:13:36 +0100

    d

---
+++ index.js
@@ -0,0 +1,8 @@
+const arrayNams = [ "John", "Bob", "Robert", "Chuck", "Pete", "Sammy", "Ana", "Sarah", "Francy", "Oscar", "Henry", "Greg"
+];
+
+const arrayNams2 = ["John", "Bobby", "Robert", "Chuck", "Peter", "Sam", "Ana", "Sarah", "Tithany", "Oscar", "Henry", "Greg"
+];
+
+const namen = arrayNams2.filter (( word) => arrayNams.includes( word));
+console.log(namen);

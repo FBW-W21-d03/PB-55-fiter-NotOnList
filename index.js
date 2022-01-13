b762902cabd1a8d446517f78664dc32750fc2226
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-13 11:41:50 +0100

    einige Änderungen

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const names2 = ["John", "Bobby", "Robert", "Chuck", "Peter", "Sam", "Ana", "Sara
 
 
 // TEIL-3
- const checking = names2.filter((name) => !["John", "Bob", "Robert", "Chuck", "Pete", "Sammy", "Sammy", "Sarah", "Francy", "Oscar", "Henry", "Greg"].includes(name));
+const checking = names2.filter((name) => !names.includes(name));
 
-console.log(checking);
+console.log("These names are not in the names array:",`${checking}`);

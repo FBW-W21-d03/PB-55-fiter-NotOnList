e4af3314b64d614d14a12701a05fb05508aab2d3
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-13 11:07:22 +0100

    Orhan's lösung

---
+++ javaScript-fiter-NotOnList.js
@@ -0,0 +1,28 @@
+// Teil 1
+const namen1 = [
+  "John",
+  "Bob",
+  "Robert",
+  "Chuck",
+  "Pete",
+  "Sammy",
+  "Ana",
+  "Sarah",
+  "Francy",
+  "Oscar",
+  "Henry",
+  "Greg",
+];
+
+// Teil 2
+const namen2 = ["John", "Bobby", "Robert", "Chuck", "Peter", "Sam", "Ana", "Sarah", "Tithany", "Oscar", "Henry", "Greg"];
+
+// Teil 3
+
+console.log(
+  namen2.includes("John", "Bob", "Robert", "Chuck",
+"Pete", "Sammy", "Ana", "Sarah", "Francy", "Oscar", "Henry", "Greg"
+  )
+);
+
+console.log(namen2)

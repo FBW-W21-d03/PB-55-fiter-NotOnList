0063e3c19c1537d6c08c66e04833607a1bda0239
kawanaza <kawanbabani@gmail.com>
2022-01-13 11:12:18 +0100

    done

--- main.js
+++ main.js
@@ -48,3 +48,36 @@ let nameObject = liste1.filter(
 //console.log(allNameObject);
 console.log(nameObject);
 console.log("nach filter ", `${nameObject}`);
+
+//// zweite lösung///
+
+const pesonen1 = [
+  "John",
+  "Bob",
+  "Robert",
+  "Chuck",
+  "Pete",
+  "Sammy",
+  "Ana",
+  "Sarah",
+  "Francy",
+  "Oscar",
+  "Henry",
+  "Greg",
+];
+const pesonen2 = [
+  "John",
+  "Bobby",
+  "Robert",
+  "Chuck",
+  "Peter",
+  "Sam",
+  "Ana",
+  "Sarah",
+  "Tithany",
+  "Oscar",
+  "Henry",
+  "Greg",
+];
+const mainListe = pesonen2.filter((elem) => !pesonen1.includes(elem));
+console.log("nach dem Filter ", `${mainListe}`);

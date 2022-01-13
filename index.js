5d79c760fdad9c4be9412c6b7314ad3bb5e0bbf1
sprotte <metatron1972@live.de>
2022-01-13 10:17:17 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const namen = [
+  "John",
+  "Bob",
+  "Robert",
+  "Chuck",
+  "Pete",
+  "Sammy",
+  "Ana",
+  "Sarah",
+  "Francy",
+  "Oscar",
+  "Henry",
+  "Greg",
+];
+
+const namen1 = [
+  "John",
+  "Bobby",
+  "Robert",
+  "Chuck",
+  "Peter",
+  "Sam",
+  "Ana",
+  "Sarah",
+  "Tithany",
+  "Oscar",
+  "Henry",
+  "Greg",
+];
